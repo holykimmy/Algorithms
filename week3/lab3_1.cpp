2bb5437732f104e50d6d78d0dfba0003759881be
holykimmy <holykimmy@holykimmys-MacBook-Pro.local>
2024-02-02 17:24:54 +0700

    edit

--- week3/lab3_1.cpp
+++ week3/lab3_1.cpp
@@ -1,81 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// int partition(int a[], int l, int r)
-// {
-//     int c = (l + r) / 2;
-//     if (a[l] > a[c]){
-//         swap(a[l], a[c]);
-//     }
-//     if (a[c] > a[r]){
-//         swap(a[c], a[r]);
-//     }
-//     if (a[l] > a[r]){
-//         swap(a[l], a[r]);
-//     }
-//     swap(a[c], a[r]);
-//     int pivot, i, j;
-//     pivot = a[r];
-
-//     i = l - 1 ;
-//     j = r ;
-//     while (1)
-//     {
-//         do
-//         {
-//             ++i;
-//         } while (a[i] <= pivot);
-
-//         do
-//         {
-//             --j;
-//         } while (a[j] > pivot);
-
-//         if (i < j)
-//             swap(a[i], a[j]);
-//         else
-//         {
-//             break;
-//         }
-//     }
-
-//     swap(a[i], a[r]);
-
-//     return  i ;
-// }
-
 int partition(int a[], int l, int r)
 {
-    int pivot, i, j, t;
-    pivot = a[l]; // first item always as the pivot
+    int pivot, i, j;
+    pivot = a[l];
     i = l;
     j = r + 1;
+
     while (1)
     {
         do
         {
             ++i;
         } while (a[i] <= pivot);
+
         do
         {
             --j;
-        } while (a[j] >= pivot);
+        } while (a[j] > pivot);
+       
+
         if (i >= j)
-            {break;}
+
+            break;
+
         swap(a[i], a[j]);
     }
+
     swap(a[l], a[j]);
+    
     return j;
 }
 
 void quickSort(int s[], int l, int r)
 {
+
+    
     int p;
     if (l < r)
     {
         p = partition(s, l, r);
+        
         quickSort(s, l, p - 1);
+        
         quickSort(s, p + 1, r);
+       
     }
 }
 
@@ -93,10 +63,11 @@ int main()
 
     // Print the sorted array
     cout << "Sorted array: ";
-    for (int i = 0; i < n ; i++)
+    for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
+    cout << endl;
 
     return 0;
 }
